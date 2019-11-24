5d57515e502f8470f9a0068325ef2523b948df08
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2019-11-24 22:45:58 +0100

    feat(master): seed pour l'épreuve

---
+++ src/data/books.js
@@ -0,0 +1,29 @@
+export const books = [
+  {
+    id: 0,
+    titre: 'La Caste des Méta-Barons T1',
+    disponible: true,
+    resume: "Quelle famille ! D'Othon, le trisaïeul, à Dona Vicenta Gabriela de Rokha, l'aïeule, le cycle des Méta-Barons conte la geste des guerriers les plus puissants de la galaxie. Cette saga dynastique mêle archaïsme et futurisme, space opera et films de samouraïs, sans jamais négliger l'humour. Une tragédie grecque sur fond de grands espaces interstellaires.",
+    auteur: 'A. Jodorowsky',
+    categorie: 'bd',
+    image: '/assets/images/la_caste_des_meta-barons.jpg',
+  },
+  {
+    id: 1,
+    titre: 'La plaisanterie',
+    disponible: true,
+    resume: "Ludvik Jahn, étudiant et militant communiste, est exclu du Parti, renvoyé de l'université et enrôlé de force dans l'armée avec les « noirs » (déviants politiques et ennemis de classe du régime socialiste tchèque) pour avoir, dans une carte postale destinée à une jeune étudiante qu'il courtisait, inscrit une phrase au second degré : « L'optimisme est l'opium du genre humain ! L'esprit sain pue la connerie ! Vive Trotski ! »",
+    auteur: 'M. Kundera',
+    categorie: 'roman',
+    image: '/assets/images/la_plaisanterie.jpg',
+  },
+  {
+    id: 2,
+    titre: 'Tractatus logico-philosophicus',
+    disponible: true,
+    resume: "Le Tractatus Logico-philosophicus est un ouvrage sur le sens. Il s'agit de tracer les limites du sens, de séparer ce qui peut être dit et ce qui ne peut pas l'être. Tout ne peut en effet être dit de façon sensée, il y a pour Wittgenstein une limite à l'expression des pensées. L'auteur ne soutient pas qu'il y a des pensées en elles-mêmes dépourvues de sens, mais plutôt que toutes les pensées ne sont pas exprimables.",
+    auteur: 'L. Wittgenstein',
+    categorie: 'essai',
+    image: '',
+  },
+];
